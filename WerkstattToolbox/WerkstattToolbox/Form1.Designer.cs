af957a2f98b87176404a3e7c82ff8a1006f7a3ed
Suppennudelchen <carsten@pohland.ch>
2018-12-09 16:04:22 +0100

    OK

--- WerkstattToolbox/WerkstattToolbox/Form1.Designer.cs
+++ WerkstattToolbox/WerkstattToolbox/Form1.Designer.cs
@@ -182,7 +182,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1052, 172);
+            this.ClientSize = new System.Drawing.Size(1052, 263);
             this.Controls.Add(this.Btn_Berechne_Vorschub);
             this.Controls.Add(this.Ausgabe_Vorschub);
             this.Controls.Add(this.label6);
