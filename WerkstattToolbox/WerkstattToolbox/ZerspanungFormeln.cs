c0f58dbec9d583a350ad01f1bb44504ed4ee8093
Suppennudelchen <carsten@pohland.ch>
2018-12-09 16:35:30 +0100

    der laufende Stand

--- WerkstattToolbox/WerkstattToolbox/ZerspanungFormeln.cs
+++ WerkstattToolbox/WerkstattToolbox/ZerspanungFormeln.cs
@@ -10,14 +10,7 @@ namespace WerkstattToolbox
     {
 
         //statische Klasse, Keyword static
-
-        //
-        // Ein kleine Aenderung
-        // oje mnie
-
-        //Ein Test für Git
-
-
+        
         //Schnittgeschwindigkeit Vc
         public static double Schnittgeschwindigkeit_Vc(double durchmesser, double drehzahl)
         {
